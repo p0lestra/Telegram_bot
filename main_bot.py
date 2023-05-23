8c7007ea7fe46f86003f60b08764a03da95750b0
Name <email@example.ru>
2023-05-23 20:40:03 +0300

    Added APIexceptions , created class with staticmethod

--- main_bot.py
+++ main_bot.py
@@ -16,6 +16,38 @@ keys = {
 }
 
 
+class APIException(Exception):
+    pass
+
+
+class MoneyConverter:
+    @staticmethod
+    def convert(to: str, base: str, amount: str):
+        if to == base:
+            raise APIException(f'Не удалось перевести одинаковые валюты {base}')
+
+        try:
+            to_ticker = keys[to]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {to}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(
+            f"https://api.apilayer.com/exchangerates_data/convert?to={to_ticker}&from={base_ticker}&amount={amount}",
+            headers=api)
+        total = json.loads(r.content)['result']
+        print(f'{total}')
+
+        return total
+
+
 @bot.message_handler(commands=['help', 'start'])
 def helper(message: telebot.types.Message):
     text = 'Чтобы перевести нужную вам валюту, напишите сообщение в таком формате: \n <имя валюты> <имя валюты, в которой надо узнать цену первой валюты> <количество первой валюты>\
@@ -34,11 +66,13 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
-    base, to, amount = message.text.split(' ')
-    r = requests.get(
-        f"https://api.apilayer.com/exchangerates_data/convert?to={keys[to]}&from={keys[base]}&amount={amount}", headers=api)
-    total = json.loads(r.content)['result']
-    print(f'{total}')
+    parametrs = message.text.split(' ')
+
+    if len(parametrs) != 3:
+        raise APIException('Слишком много значений')
+    base, to, amount = parametrs
+
+    total = MoneyConverter.convert(to, base, amount)
     text = f'Цена {amount} {base} в {to} = {total}'
     bot.send_message(message.chat.id, text)
 
